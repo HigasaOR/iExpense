3c5e474e57c1a219f646b66f1736b160993a151c
HigasaOR <lee1003094395@gmail.com>
2024-07-25 14:37:20 +0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Chien Lee on 2024/7/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
