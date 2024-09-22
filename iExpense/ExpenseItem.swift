c26def105b353cf239195031080569bda268f29a
HigasaOR <lee1003094395@gmail.com>
2024-09-22 20:49:37 +0800

    finish project 12

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Chien Lee on 2024/9/22.
+//
+
+import Foundation
---
+++ iExpense/ExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseView.swift
+//  iExpense
+//
+//  Created by Chien Lee on 2024/9/22.
+//
+
+import SwiftUI
+
+struct ExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseView()
+}
